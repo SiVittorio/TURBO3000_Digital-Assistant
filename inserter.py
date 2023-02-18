d109ce49010a11ef4e8d4d04063cb9624a89c2ba
SiVittorio <62559964+SiVittorio@users.noreply.github.com>
2023-02-19 01:41:01 +0300

    скрипт для добавления данных в таблицу бд

---
+++ inserter.py
@@ -0,0 +1,20 @@
+import psycopg2
+
+def selectAll():
+    cursor.execute('SELECT * FROM requests')
+    requests = cursor.fetchall()
+    print(requests)
+
+def insertInto(idUser, userName, request):
+    sql = 'INSERT INTO requests (id_Users, user_name, request) VALUES (' + str(idUser) + ', \'' + userName + '\'' + ', \'' + request + '\')'
+    cursor.execute(sql)
+    selectAll()
+
+conn = psycopg2.connect(dbname='grants.myrosmol', user='postgres', 
+                        password='3568', host='localhost')
+with conn.cursor() as cursor:
+    conn.autocommit = True
+    insertInto(input(), input(), input())
+
+conn.close()
+
